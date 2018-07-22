083f26507edcfa502403848a716b2fd6a05ff6f6
Eastroots92 <eastroots92@gmail.com>
2018-07-22 20:22:10 +0900

    Rename package and restructure directories
    - Presenter/View layer를 생성한다.
    - 복수형 name을 단수로 수정한다.
    - 접근 생성자 e.g. private... 등을 추가한다.
    
    Fixes https://github.com/MashUp-Android/MashUp-Android/issues/10

--- app/src/main/java/com/openull/eastroots92/vacation_homework_android/ui/main/fragment/setting/SettingPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.openull.eastroots92.vacation_homework_android.ui.main.fragment.setting;
-
-public class SettingPresenter {
-  private SettingFragment view;
-
-  public SettingPresenter(SettingFragment view) {
-    this.view = view;
-  }
-}
