0387b1f41e5ce7cae977b0c7d4d32b59af0cb8a0
Eastroots92 <eastroots92@gmail.com>
2018-08-15 19:50:02 +0900

    Setup buttonListener in settingActivity
    settingActivity에 각각의 textView에 Listener를 달아준다.

--- app/src/main/java/com/chatty/android/chattyClient/view/setting/SettingActivity.java
+++ app/src/main/java/com/chatty/android/chattyClient/view/setting/SettingActivity.java
@@ -2,14 +2,31 @@ package com.chatty.android.chattyClient.view.setting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.chatty.android.chattyClient.R;
 import com.chatty.android.chattyClient.presenter.setting.SettingPresenter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingActivity extends AppCompatActivity {
   private SettingPresenter presenter;
+
+  @BindView(R.id.textView_button_link_account)
+  public TextView textViewButtonLinkAccount;
+
+  @BindView(R.id.textView_button_friends_setting)
+  public TextView textViewButtonFriendsSetting;
+
+  @BindView(R.id.textView_button_add_question)
+  public TextView textViewButtonAddQuestion;
+
+  @BindView(R.id.textView_button_notification)
+  public TextView textViewButtonNotification;
+
+  @BindView(R.id.textView_button_support)
+  public TextView textViewButtonSupport;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +42,34 @@ public class SettingActivity extends AppCompatActivity {
   }
 
   public void render() {
+    buttonRender();
+  }
+
+  private void buttonRender() {
+    renderLinkAccountButton();
+    renderFriendsSettingButton();
+    renderAddQuestionButton();
+    renderNotificationButton();
+    renderSupportButton();
+  }
+
+  private void renderLinkAccountButton() {
+    textViewButtonLinkAccount.setOnClickListener((__) -> {});
+  }
+
+  private void renderFriendsSettingButton() {
+    textViewButtonFriendsSetting.setOnClickListener((__) -> {});
+  }
+
+  private void renderAddQuestionButton() {
+    textViewButtonAddQuestion.setOnClickListener((__) -> {});
+  }
+
+  private void renderNotificationButton() {
+    textViewButtonNotification.setOnClickListener((__) -> {});
+  }
+
+  private void renderSupportButton() {
+    textViewButtonSupport.setOnClickListener((__) -> {});
   }
 }
