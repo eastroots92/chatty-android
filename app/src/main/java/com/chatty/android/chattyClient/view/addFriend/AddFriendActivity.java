47bd534408eb910ccbdcc92ffa8a6a0803fa00e3
Eastroots92 <eastroots92@gmail.com>
2018-08-31 22:14:13 +0900

    Set button action
    각각의 버튼 클릭 리스너들을 만든다.

--- app/src/main/java/com/chatty/android/chattyClient/view/addFriend/AddFriendActivity.java
+++ app/src/main/java/com/chatty/android/chattyClient/view/addFriend/AddFriendActivity.java
@@ -2,14 +2,29 @@ package com.chatty.android.chattyClient.view.addFriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.chatty.android.chattyClient.R;
 import com.chatty.android.chattyClient.presenter.addFriend.AddFriendPresenter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddFriendActivity extends AppCompatActivity {
+  private static String HEADER_TITLE = "Add Friend";
   AddFriendPresenter presenter;
+
+  @BindView(R.id.button_timeline_left)
+  public ImageButton imageButtonBack;
+
+  @BindView(R.id.imageView_profile)
+  public ImageView imageViewProfile;
+
+  @BindView(R.id.imageView_addProfile_button)
+  public ImageView imageViewAddProfileButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +40,45 @@ public class AddFriendActivity extends AppCompatActivity {
   }
 
   public void render() {
+    renderHeader();
+    renderForm();
   }
+
+  private void renderForm() {
+    renderProfileImage();
+  }
+
+  private void renderProfileImage() {
+    profileImageButtonManager();
+  }
+
+  private void profileImageButtonManager() {
+    imageViewProfile.setOnClickListener(view -> {
+      profileImageButtonAction();
+    });
+    imageViewAddProfileButton.setOnClickListener(view -> {
+      profileImageButtonAction();
+    });
+  }
+
+  private void profileImageButtonAction() {
+    Log.v("동작", "동작완료");
+  }
+
+  private void renderHeader() {
+    setHeaderTitle();
+    renderBackButton();
+  }
+
+  private void setHeaderTitle() {
+    TextView textView = findViewById(R.id.textView_timeline_title);
+    textView.setText(HEADER_TITLE);
+  }
+
+  private void renderBackButton() {
+    imageButtonBack.setOnClickListener((__) -> {
+      finish();
+    });
+  }
+
 }
