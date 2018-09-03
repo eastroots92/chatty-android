1252b0faf4058c4c37b9ccb3ec999bb7335b0340
Eastroots92 <eastroots92@gmail.com>
2018-09-04 01:44:14 +0900

    Connect FriendsListAcitivty to FriendsSetting
    FriendsListAcitivty와 FriendsSetting를 연결한다.
    
    Fixes https://github.com/chatty-app/chatty-app/issues/60

--- app/src/main/java/com/chatty/android/chattyClient/view/friendsList/FriendsListActivity.java
+++ app/src/main/java/com/chatty/android/chattyClient/view/friendsList/FriendsListActivity.java
@@ -1,5 +1,6 @@
 package com.chatty.android.chattyClient.view.friendsList;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
