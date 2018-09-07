35b46a1f85309a2d1e5e14f9f732c9844735a71b
Eastroots92 <eastroots92@gmail.com>
2018-09-08 02:37:47 +0900

    Initialize splashActivity
    SplashActivity 기초 토대를 생성한다.

---
+++ app/src/main/java/com/chatty/android/chattyClient/view/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.chatty.android.chattyClient.view.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chatty.android.chattyClient.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+  }
+}
